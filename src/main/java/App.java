7b05e978d33d72d7a0923379eb72f62bbaeb152d
mati26 <mati_b@op.pl>
2017-04-08 09:00:38 +0200

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,10 @@
+import lombok.Getter;
+
+/**
+ * Created by Mateusz on 2017-04-07.
+ */
+public class App {
+    public static void main(String[] args){
+        System.out.println("Hello");
+    }
+}
---
+++ src/main/java/Entity.java
@@ -0,0 +1,10 @@
+import lombok.Data;
+
+/**
+ * Created by Mateusz on 2017-04-08.
+ */
+@Data
+public class Entity {
+    private Long id;
+    private String rest;
+}
